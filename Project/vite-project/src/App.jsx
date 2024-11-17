fda3c903e7a645feb53fa984cdba85eb696c234d
Tris-Dream <tj2001john@gmail.com>
2024-11-17 15:38:05 +1100

    fetching  data from api

--- Project/vite-project/src/App.jsx
+++ Project/vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import FilmFetching from "../components/Fetching";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <FilmFetching />
+    </>
+  );
 }
 
 export default App;
